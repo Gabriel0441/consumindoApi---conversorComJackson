64df084e92888af98b6d29d7b4e9baff3e315379
Gabriel Coutinho <bielg8998@gmail.com>
2026-09-10 22:53:47 -0300

    Framework Spring

---
+++ screenmatch/src/main/java/br/com/coutin/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.coutin.screenmatch;
+
+import br.com.coutin.screenmatch.model.DadosSerie;
+import br.com.coutin.screenmatch.service.ConsumoApi;
+import br.com.coutin.screenmatch.service.converteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?i=gilmore+girls&Season=1&apikey=161602d9");
+		System.out.println(json);
+        converteDados conversor = new converteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
---
+++ screenmatch/src/main/java/br/com/coutin/screenmatch/service/converteDados.java
@@ -0,0 +1,14 @@
+package br.com.coutin.screenmatch.service;
+
+import tools.jackson.databind.ObjectMapper;
+
+public class converteDados implements IConverteDados {
+    private ObjectMapper mapper =  new ObjectMapper();
+
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        return mapper.readValue(json, classe);
+
+    }
+}
